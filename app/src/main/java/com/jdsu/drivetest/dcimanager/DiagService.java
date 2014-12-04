e67a6cab0f73de3496d1997061448de8b923186a
SiMing Weng <siming.weng@gmail.com>
2014-12-04 09:54:28 +0800

    add client registration and query of supported list

--- app/src/main/java/com/jdsu/drivetest/dcimanager/DiagService.java
+++ app/src/main/java/com/jdsu/drivetest/dcimanager/DiagService.java
@@ -6,10 +6,13 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.jdsu.drivetest.diag.DiagLibrary;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.ShortByReference;
 
 public class DiagService extends Service {
 
     private static final String TAG = DiagService.class.getName();
+    private IntByReference client_id;
 
     public DiagService() {
     }
@@ -24,13 +27,35 @@ public class DiagService extends Service {
     public void onCreate() {
         super.onCreate();
         int result = DiagLibrary.INSTANCE.Diag_LSM_Init(null);
-        Log.i(TAG, "LSM initialization result " + result);
+        Log.i(TAG, "LSM initialization result code" + result);
+        client_id = new IntByReference();
+        short[] peripherals = {0x0003};
+        int[] os_params = {18};
+        result = DiagLibrary.INSTANCE.diag_register_dci_client(client_id, peripherals, 0, os_params);
+        if (result != 1001) {
+            Log.e(TAG, "Failed to register DCI client result code " + result);
+            return;
+        }
+        Log.i(TAG, "Register DCI client successfully, client id " + client_id.getValue());
+
+        ShortByReference list = new ShortByReference();
+        result = DiagLibrary.INSTANCE.diag_get_dci_support_list(list);
+        if (result != 1001) {
+            Log.e(TAG, "failed to get supported list, result code " + result);
+            return;
+        }
+        Log.i(TAG, "APSS Status: " + ((list.getValue() & 0x0001) == 1 ? "Up" : "Down"));
+        Log.i(TAG, "MPSS Status: " + ((list.getValue() & 0x0002) == 2 ? "Up" : "Down"));
+        Log.i(TAG, "LPASS Status: " + ((list.getValue() & 0x0004) == 4 ? "Up" : "Down"));
+        Log.i(TAG, "WCNSS Status: " + ((list.getValue() & 0x0008) == 8 ? "Up" : "Down"));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         int result = DiagLibrary.INSTANCE.Diag_LSM_DeInit();
-        Log.i(TAG, "LSM deinitialization result " + result);
+        Log.i(TAG, "LSM deinitialization result code" + result);
+        result = DiagLibrary.INSTANCE.diag_release_dci_client(client_id);
+        Log.i(TAG, "release DCI client result code " + result);
     }
 }
--- app/src/main/java/com/jdsu/drivetest/diag/DiagLibrary.java
+++ app/src/main/java/com/jdsu/drivetest/diag/DiagLibrary.java
@@ -2,8 +2,11 @@ package com.jdsu.drivetest.diag;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.ShortByReference;
 
 /**
+ * The JNA mapping of the libdiag.so
  * Created by wen55527 on 3/12/14.
  */
 public interface DiagLibrary extends Library {
@@ -13,4 +16,10 @@ public interface DiagLibrary extends Library {
     int Diag_LSM_Init(byte[] pIEnv);
 
     int Diag_LSM_DeInit();
+
+    int diag_register_dci_client(IntByReference client_id, short[] list, int channel, int[] os_params);
+
+    int diag_release_dci_client(IntByReference client_id);
+
+    int diag_get_dci_support_list(ShortByReference list);
 }
